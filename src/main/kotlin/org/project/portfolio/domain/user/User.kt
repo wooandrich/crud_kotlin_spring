26fb52ddb64743a37ab8c6a45318d289fe37e5a7
wooandrich <brad1182@naver.com>
2024-08-05 17:30:17 +0900

    refactor : User 테이블명 h2데이터베이스 사용으로 인한 변경

--- src/main/kotlin/org/project/portfolio/domain/user/User.kt
+++ src/main/kotlin/org/project/portfolio/domain/user/User.kt
@@ -1,12 +1,10 @@
 package org.project.portfolio.domain.user
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import org.project.portfolio.global.common.domain.BaseEntity
 
 @Entity
+@Table(name = "users")
 class User : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
