53c4102f7a3fd7a4bf11f1d1f9d6ffbc154c7bc0
wooandrich <brad1182@naver.com>
2024-08-05 21:14:57 +0900

    docs : kotlin jpa 설정수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
+    val kotlinVersion = "1.9.24"
     id("org.springframework.boot") version "3.3.1"
     id("io.spring.dependency-management") version "1.1.5"
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.allopen") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
 }
 
 group = "org.project"
@@ -34,7 +37,25 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.2")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.2")
+    // swagger 설정
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.15")
+    implementation("org.springdoc:springdoc-openapi-security:1.6.15")
+
 }
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+}
+
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
+
+
+
+
+
 
 kotlin {
     compilerOptions {
